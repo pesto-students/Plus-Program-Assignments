64862e45907dec1d26b9651f82c929b5c60a585b
Ketan Santosh Walunjkar <ketanwalunjkar12@gmail.com>
2024-01-10 15:11:43 +0530

    Seesion 9 assignment

--- Web Fundamentals/Session-09_JS_Basics/Assignments/JavaScript arrays/script.js
+++ Web Fundamentals/Session-09_JS_Basics/Assignments/JavaScript arrays/script.js
@@ -1,1 +1,13 @@
 // Put your solution here
+
+function divideArray(nums) {
+    let evenNums = [], oddNums = [];
+    for (let i in nums) {
+        if (nums[i] % 2 === 0) {
+            evenNums.push(nums[i])
+        } else {
+            oddNums.push(nums[i])
+        }
+    }
+    console.log('Even numbers :', evenNums.length > 0 ? evenNums.sort() : 'None', 'Odd numbers  :', oddNums.length > 0 ? oddNums.sort() : 'None')
+}
